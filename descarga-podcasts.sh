0b29937344d195549d14b8569d765da0b553f4ac
miguelanxo <miguelanxo@outlook.com>
2022-08-31 23:10:06 +0200

    Actualizacion 31/08/22
    Engadido comando descarga 'Opiniones en Txuri Urdin'

--- descarga-podcasts.sh
+++ descarga-podcasts.sh
@@ -515,6 +515,12 @@ FICHERO_DESTINO="/home/cuacfm/ownCloud/Podcast/CUACFM/Vellenials/vellenials.mp3"
 DURACION_MINIMA="0"
 /home/cuacfm/bin/descarga-primer-podcast-programa.sh "$PODCAST_RSS_URL" "$FICHERO_DESTINO" "$DURACION_MINIMA"
 
+#DESCARGA DE OPINIONES EN TXURI URDIN
+PODCAST_RSS_URL="https://cuacfm.org/radioco/programmes/opiniones-en-txuri-urdin/rss/"
+FICHERO_DESTINO="/home/cuacfm/ownCloud/Podcast/CUACFM/Opiniones-En-Txuri-Urdin/opinionestxuriurdin.mp3"
+DURACION_MINIMA="0"
+/home/cuacfm/bin/descarga-primer-podcast-programa.sh "$PODCAST_RSS_URL" "$FICHERO_DESTINO" "$DURACION_MINIMA"
+
 
 #PETICIONES PODCAST ONDA COLOR
 # DESCARGA SANGRE FUCSIA PARA ONDACOLOR
